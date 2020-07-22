fc8761e52da14d5a6a3d413277c05b3f8b669865
y24er <1028704486@qq.com>
2020-07-22 21:33:06 +0800

    [practice08][Jessie] feature : add leader

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,30 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
 }
