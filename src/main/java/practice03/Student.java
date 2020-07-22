5bebe678967a53c833aa02b1e3b05077b0b1d126
y24er <1028704486@qq.com>
2020-07-22 20:44:22 +0800

    [practice03][Jessie] refactor : Student()

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,23 +1,23 @@
 package practice03;
 
 public class Student extends Person{
-    private String klass;
+    private int klass;
 
-    public Student(String name, int age) {
+    public Student(String name, int age, int klass) {
         super(name, age);
+        this.klass = klass;
     }
 
-
-    public String getKlass() {
+    public int getKlass() {
         return klass;
     }
 
-    public void setKlass(String klass) {
+    public void setKlass(int klass) {
         this.klass = klass;
     }
 
     @Override
     public String introduce() {
-        return "I am a Student. I am at "+klass+".";
+        return "I am a Student. I am at Class "+klass+".";
     }
 }
