6f33979da535fce6e25eb6ef5db1fefacc1fec23
y24er <1028704486@qq.com>
2020-07-22 17:47:11 +0800

    [practice07][Jessie] feature : add introduceWith(Student)

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,36 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null)
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        return super.introduce() + " I am a Teacher. I teach Class " + klass.getNumber() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().equals(klass)) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }
