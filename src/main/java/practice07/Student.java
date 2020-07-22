427fedb74365bebc166807fbf2814ea5f69e02aa
y24er <1028704486@qq.com>
2020-07-22 17:47:39 +0800

    [practice07][Jessie] feature : add introduce()

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,27 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
 }
