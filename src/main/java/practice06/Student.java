2bce4bfe1dd21847e52dc7b8615b102157731b6b
y24er <1028704486@qq.com>
2020-07-22 16:15:39 +0800

    [practice06][Jessie] feature : add introduce() and call super.introduce()

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,23 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+    private String klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+
+    public String getKlass() {
+        return klass;
+    }
+
+    public void setKlass(String klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + "I am a Student. I am at " + klass + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private String klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String getKlass() {
+        return klass;
+    }
+
+    public void setKlass(String klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null)
+            return super.introduce() + "I am a Teacher. I teach No Class.";
+        return super.introduce() + "I am a Teacher. I teach " + klass + ".";
+    }
+
 }
