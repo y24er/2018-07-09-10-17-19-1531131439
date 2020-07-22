30872184318e70e0b85fd82073c98ed364608e26
y24er <1028704486@qq.com>
2020-07-22 22:57:58 +0800

    [practice10][Jessie] feature : introduce()

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,51 @@
 package practice10;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj instanceof Person) {
+            return this.id == ((Person) obj).getId();
+        } else
+            return false;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
 }
