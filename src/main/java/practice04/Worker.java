5f8adf2c465ea8db7de1a4e4a935c4d7510a6e55
y24er <1028704486@qq.com>
2020-07-22 16:01:39 +0800

    [practice04][Jessie] feature : add introduce() and call baseIntroduce()

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,13 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return basicIntroduce()+"I am a Worker. I have a job.";
+    }
 }
+
