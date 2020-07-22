baade0229dcaba7eb91746674bcf456d6c7c6614
y24er <1028704486@qq.com>
2020-07-22 21:07:25 +0800

    [practice06][Jessie] refactor : introduce()

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,25 +1,30 @@
 package practice06;
 
 public class Teacher extends Person {
-    private String klass;
+    private int klass = 0;
 
     public Teacher(String name, int age) {
         super(name, age);
     }
 
-    public String getKlass() {
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
         return klass;
     }
 
-    public void setKlass(String klass) {
+    public void setKlass(int klass) {
         this.klass = klass;
     }
 
     @Override
     public String introduce() {
-        if (klass == null)
-            return super.introduce() + "I am a Teacher. I teach No Class.";
-        return super.introduce() + "I am a Teacher. I teach " + klass + ".";
+        if (klass == 0)
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
     }
 
 }
