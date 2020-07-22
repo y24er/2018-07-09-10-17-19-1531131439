fd4eadb330669b67b5c9c9fab135b448cf895622
y24er <1028704486@qq.com>
2020-07-22 16:08:55 +0800

    [practice05][Jessie] feature : add introduce() and call super.introduce()

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,24 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    private String klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+
+    public String getKlass() {
+        return klass;
+    }
+
+    public void setKlass(String klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + "I am a Student. I am at " + klass + ".";
+    }
 }
+
