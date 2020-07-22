4079d555af19611c437dbcc8456019d2f770f8c7
y24er <1028704486@qq.com>
2020-07-22 22:58:14 +0800

    [practice10][Jessie] feature : introduce()

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,30 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String result = super.introduce() + " I am a Student.";
+        Student leader = this.getKlass().getLeader();
+        if (leader != null) {
+            if (this.equals(leader)) {
+                return result + String.format(" I am Leader of Class %d.", klass.getNumber());
+            }
+        }
+        return result + String.format(" I am at Class %d.", klass.getNumber());
+    }
 }
