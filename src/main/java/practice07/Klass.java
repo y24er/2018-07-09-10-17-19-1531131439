3c75717c1af02789c595d4093ecd8213658ef38a
y24er <1028704486@qq.com>
2020-07-22 17:46:16 +0800

    [practice07][Jessie] feature : add Klass.java

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,24 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Klass() {
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
 }
